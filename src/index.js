f65cd32885d8821a6f5d17a35b26923884ac93d6
Dekel <dekel@lusha.co>
2018-04-23 18:37:22 +0300

    Removed all logic related to verifying Stripe signatures

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import Debug from 'debug';
-import stripe from 'stripe';
-
 import service from './service';
 
 const debug = Debug('feathers-stripe-webhooks:main');
 
-function checkOpts(options) {
-  if (options.verifyEvents && !options.secret) {
-    throw new Error('Stripe secret key (options.secret) is required when verifyEvents is true.');
-  }
-}
+export default function feathersStripeWebhooks(handlers) {
+  debug('Creating feathers-stripe-webhooks service');
 
-export default function feathersStripeWebhooks(handlers, config = {}) {
-  const options = Object.assign(
-    {
-      // Set to false to disable event fetching from Stripe
-      // https://stripe.com/docs/webhooks#verifying-events
-      verifyEvents: true,
-    },
-    config
-  );
-
-  debug(`Creating feathers-stripe-webhooks service with options: ${JSON.stringify(options)}`);
-
-  checkOpts(options);
-
-  const api = options.secret ? stripe(options.secret) : null;
-  return service(api, handlers, options);
+  return service(handlers);
 }
